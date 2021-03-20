d85aca400e59a8cb93873480b746b4d17774eca5
Olga Skliarova <skliarovaolga77@gmail.com>
2021-03-20 19:32:33 +0200

    два списка дел TaskManager

---
+++ ДЗ 30. 28/katya.js
@@ -0,0 +1,71 @@
+Vue.component('wrapper',{
+
+  data(){
+    return {
+      tasks:['покушать','помыть посуду','погулять','сделать домашку ','посмотреть фильм'],
+      tasks2:[]
+    }
+  },
+  
+  template:`<div class="wrapper">
+              <first-list :tasks_list="tasks" :tasks_list2="tasks2"></first-list>
+			        <second-list :tasks_list="tasks" :tasks_list2="tasks2"></second-list>
+            </div>`
+
+}),
+Vue.component('first-list',{
+  props: {
+    tasks_list:{
+      type:Array,
+      default: []
+    },
+    tasks_list2:{
+      type:Array,
+      default: []
+    }
+  },
+  template:`<div class="first-list">
+              <ul>
+                 <li v-for="task in tasks_list">{{task}}</li>
+              </ul>
+              <button class="transfer" @click="removeItem">transfer</button>
+            </div>`,
+  methods: {
+   removeItem(){
+      let addedValue = this.tasks_list.pop();
+      this.tasks_list2.push(addedValue);
+    }
+  }
+
+}),
+Vue.component('second-list',{
+   props: {
+    tasks_list:{
+      type:Array,
+      default: []
+    },
+    tasks_list2:{
+      type:Array,
+      default: []
+    }
+  },
+  template:`<div class="second-list">
+              <ul>
+                <li v-for="task in tasks_list2">{{task}}</li>
+              </ul>
+              <button class="transfer" @click="addItem">transfer</button>
+            </div>`,
+  methods: {
+    addItem(){
+      let addedValue = this.tasks_list2.shift();
+      this.tasks_list.push(addedValue);
+    }
+  }
+
+})
+
+
+let taskManager = new Vue({
+  el:'#taskManager',
+  data:{}
+});
--- ДЗ 30. 28/main.js
+++ ДЗ 30. 28/main.js
@@ -1,64 +1,74 @@
-Vue.component('wrapper',{
+Vue.component('wrapper', {
 
-  data(){
-    return {
-      tasks:['покушать','помыть посуду','погулять','сделать домашку ','посмотреть фильм'],
-      tasks2:[]
-    }
-  },
-  
-  template:`<div class="wrapper">
-              <first-list :tasks_list="tasks"></first-list>
-			        <second-list :tasks_list2="tasks2"></second-list>
+    data() {
+      return {
+        tasks: ['покушать', 'помыть посуду', 'погулять', 'сделать домашку ', 'посмотреть фильм'],
+        tasks2: []
+      };
+    },
+
+    template: `<div class="wrapper">
+              <first-list :tasks_list="tasks" :tasks_list2="tasks2"></first-list>
+			        <second-list :tasks_list="tasks" :tasks_list2="tasks2"></second-list>
             </div>`
 
-}),
-Vue.component('first-list',{
-  props: {
-    tasks_list:{
-      type:Array,
-      default: []
-    }
-  },
-  template:`<div class="first-list">
+  }),
+  Vue.component('first-list', {
+    props: {
+      tasks_list: {
+        type: Array,
+        default: []
+      },
+      tasks_list2: {
+        type: Array,
+        default: []
+      }
+    },
+    template: `<div class="first-list">
               <ul>
                  <li v-for="task in tasks_list">{{task}}</li>
               </ul>
               <button class="transfer" @click="removeItem">transfer</button>
             </div>`,
-  methods: {
-   removeItem(){
-      let addedValue = this.tasks_list.pop();
-      this.addItem(addedValue);
+    methods: {
+      removeItem() {
+        //shift удаляем последний элемент
+        let addedValue = this.tasks_list.pop();
+        this.tasks_list2.push(addedValue);
+      }
+    }
+
+  }),
+  Vue.component('second-list', {
+    props: {
+      tasks_list: {
+        type: Array,
+        default: []
+      },
+      tasks_list2: {
+        type: Array,
+        default: []
+      }
     },
-    addItem(addedValue){
-      console.log(addedValue);
-      return addedValue;
-    }
-  }
-
-}),
-Vue.component('second-list',{
-   props: {
-    tasks_list:{
-      type:Array,
-      default: []
-    }
-  },
-  template:`<div class="second-list">
+    template: `<div class="second-list">
               <ul>
-                <li v-for="task in tasks_list">{{task}}</li>
+                <li v-for="task in tasks_list2">{{task}}</li>
               </ul>
+               <button class="transfer" @click="addItem">transfer</button>
             </div>`,
-  methods: {
-    addItem(addedValue){
+    methods: {
+      addItem() {
+        console.log(this.tasks_list2);
+        //shift удаляем первый элемент
+        let addedValue = this.tasks_list2.shift();
+        this.tasks_list.push(addedValue);
+      }
     }
-  }
 
-})
+  })
 
 
 let taskManager = new Vue({
-  el:'#taskManager',
-  data:{}
+  el: '#taskManager',
+  data: {}
 });
