0b28aa7a4bea3b281fe336e3fea79b06f11e5d66
Olga Skliarova <skliarovaolga77@gmail.com>
2021-03-21 19:00:55 +0200

    dz

--- ДЗ 31. Эквалайзер/script.js
+++ ДЗ 31. Эквалайзер/script.js
@@ -1,24 +1,35 @@
 Vue.component('elements', {
-
-   data() {
-      return {
-       
-      };
+  props: {
+    my_colors:{
+      default: []
     },
-
+    methods: {
+      classColor(){
+        console.log(this.my_colors);
+        //v-for="color in my_colors"
+        this.my_colors.forEach((color)=>{
+          console.log(color);
+          return color;
+        });
+      }
+    }
+  },
 
   template:`<div class="wrapper">
-              <div class="block"></div>
+              <div class="block" ></div>
+              <div class="block-color"></div>
               <input type="range" id="range" min="1" value="1" step="1" />
           </div>`,
   methods: {
     
+    
   }
-})
-
+});
 
 
 let Equalizer = new Vue({
   el: '#equalizer',
-  data: {}
+  data: {
+    	colors: ['red','blue','green','black', 'orange', 'bardo', 'pink']
+  }
 });
